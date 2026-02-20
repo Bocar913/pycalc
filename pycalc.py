44f4cf88c29c269c3ca3583a9dada3a037b92ca9
Bocar Thiaw <22405034@me.uvsq.fr>
2026-02-20 15:39:40 +0100

    addition

--- pycalc.py
+++ pycalc.py
@@ -5,14 +5,4 @@ b = float(sys.argv[3])
 op = sys.argv[2]
 if op == "+":
     print(a+b)
-elif op == "-":
-    print(a-b)
-elif op == "/":
-    if b == 0 :
-        print("impossible")
-    else :
-        print(a/b)
-elif op == "*":
-    print (a*b)
-else :
-    print("Opérateur non valide")
+
