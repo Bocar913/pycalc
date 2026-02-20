78b0e56fb4fad9d82f6d6b3b2e2d23d7eebd5ccd
Bocar Thiaw <22405034@me.uvsq.fr>
2026-02-20 15:15:06 +0100

    mise à jour

--- pycalc.py
+++ pycalc.py
@@ -1,1 +1,16 @@
-d
+a = 
+b = 
+op = ""
+if op == "+":
+    print(a+b)
+elif op == "-":
+    print(a-b)
+elif op == "/":
+    if b == 0 :
+        print("impossible")
+    else :
+        print(a/b)
+elif op == "*":
+    print (a*b)
+else :
+    print("Opérateur non valide")
